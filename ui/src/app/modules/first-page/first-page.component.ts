348aa6e4f4e976f29702f5e7dcd6102a5b276125
Shital Kisan Shirole <shitalsh@cybage.com>
2022-12-22 16:28:11 +0530

    ui changes

--- ui/src/app/modules/first-page/first-page.component.ts
+++ ui/src/app/modules/first-page/first-page.component.ts
@@ -18,83 +18,74 @@ import {
   templateUrl: './first-page.component.html',
   styleUrls: ['./first-page.component.scss'],
 })
-export class FirstPageComponent {
-  // public customForm: FormGroup;
+export class FirstPageComponent implements OnInit {
+  public customForm: FormGroup;
   public frontendFrameworkList = frontendFramework;
   public backendFrameworkList = backendFramework;
-  toppings = new FormControl('');
-  toppingList: string[] = [
-    'Extra cheese',
-    'Mushroom',
-    'Onion',
-    'Pepperoni',
-    'Sausage',
-    'Tomato',
-  ];
-  // public moduleListForm: any[] = ['Add New One'];
-  // public tableData: IApplicationDetails[] = [];
-  // public componentTypes = componentTypes;
-  // public customerAirCarrierCodes = 'customerAirCarrierCodes';
-  // constructor(public fb: FormBuilder) {
-  //   this.customForm = this.fb.group({
-  //     frontendFramework: new FormControl(null, Validators.required),
-  //     backendFramework: new FormControl(null, Validators.required),
-  //     moduleName: new FormControl('', Validators.required),
-  //     componentName: new FormControl('', Validators.required),
-  //     componentType: new FormControl('', Validators.required),
-  //     customerAirCarrierCodes: new FormArray([]),
-  //   });
-  // }
-  // ngOnInit(): void {}
-  // get f(): any {
-  //   return this.customForm.controls;
-  // }
-  // public triggerValidation(): boolean {
-  //   this.customForm.markAllAsTouched();
-  //   return this.customForm.valid ? true : false;
-  // }
-  // addInput(): void {
-  //   const form = this.customForm.value;
-  //   if (this.triggerValidation()) {
-  //     const saveObj: IApplicationDetails = {
-  //       frontendFramework: form.frontendFramework,
-  //       backendFramework: form.backendFramework,
-  //       moduleName: form.moduleName,
-  //       componentName: form.componentName,
-  //       componentType: form.componentType,
-  //       fieldDetails: form.customerAirCarrierCodes,
-  //     };
-  //     if (saveObj.moduleName !== 'Add New One')
-  //       this.moduleListForm.push(saveObj.moduleName);
-  //     this.tableData.push(saveObj);
-  //     this.customForm.reset();
-  //     const arrayControl = <FormArray>(
-  //       this.customForm.controls[this.customerAirCarrierCodes]
-  //     );
-  //     arrayControl.clear();
-  //   }
-  // }
-  // get airCarrierArr(): Array<FormGroup> {
-  //   return <FormGroup[]>(
-  //     (this.customForm.get(this.customerAirCarrierCodes) as FormArray).controls
-  //   );
-  // }
-  // addFieldInput(): void {
-  //   const arrayControl = <FormArray>(
-  //     this.customForm.controls[this.customerAirCarrierCodes]
-  //   );
-  //   this.createNewFormGroup(arrayControl);
-  // }
-  // createNewFormGroup(arrayControl: FormArray): void {
-  //   const addFieldForm = this.fb.group({
-  //     fieldNameFET: new FormControl('', Validators.required),
-  //     fieldNameBackend: new FormControl('', Validators.required),
-  //     lengthOfField: new FormControl('', Validators.required),
-  //     typeOfField: new FormControl('', Validators.required),
-  //     validation: new FormControl('', Validators.required),
-  //   });
+  public moduleListForm: any[] = ['Add New One'];
+  public tableData: IApplicationDetails[] = [];
+  public componentTypes = componentTypes;
+  public customerAirCarrierCodes = 'customerAirCarrierCodes';
+  constructor(public fb: FormBuilder) {
+    this.customForm = this.fb.group({
+      frontendFramework: new FormControl(null, Validators.required),
+      backendFramework: new FormControl(null, Validators.required),
+      moduleName: new FormControl('', Validators.required),
+      componentName: new FormControl('', Validators.required),
+      componentType: new FormControl('', Validators.required),
+      customerAirCarrierCodes: new FormArray([]),
+    });
+  }
+  ngOnInit(): void {}
+  get f(): any {
+    return this.customForm.controls;
+  }
+  public triggerValidation(): boolean {
+    this.customForm.markAllAsTouched();
+    return this.customForm.valid ? true : false;
+  }
+  addInput(): void {
+    const form = this.customForm.value;
+    if (this.triggerValidation()) {
+      const saveObj: IApplicationDetails = {
+        frontendFramework: form.frontendFramework,
+        backendFramework: form.backendFramework,
+        moduleName: form.moduleName,
+        componentName: form.componentName,
+        componentType: form.componentType,
+        fieldDetails: form.customerAirCarrierCodes,
+      };
+      if (saveObj.moduleName !== 'Add New One')
+        this.moduleListForm.push(saveObj.moduleName);
+      this.tableData.push(saveObj);
+      this.customForm.reset();
+      const arrayControl = <FormArray>(
+        this.customForm.controls[this.customerAirCarrierCodes]
+      );
+      arrayControl.clear();
+    }
+  }
+  get airCarrierArr(): Array<FormGroup> {
+    return <FormGroup[]>(
+      (this.customForm.get(this.customerAirCarrierCodes) as FormArray).controls
+    );
+  }
+  addFieldInput(): void {
+    const arrayControl = <FormArray>(
+      this.customForm.controls[this.customerAirCarrierCodes]
+    );
+    this.createNewFormGroup(arrayControl);
+  }
+  createNewFormGroup(arrayControl: FormArray): void {
+    const addFieldForm = this.fb.group({
+      fieldNameFET: new FormControl('', Validators.required),
+      fieldNameBackend: new FormControl('', Validators.required),
+      lengthOfField: new FormControl('', Validators.required),
+      typeOfField: new FormControl('', Validators.required),
+      validation: new FormControl('', Validators.required),
+    });
 
-  //   arrayControl.push(addFieldForm);
-  //   this.customForm.controls[this.customerAirCarrierCodes] = arrayControl;
-  // }
+    arrayControl.push(addFieldForm);
+    this.customForm.controls[this.customerAirCarrierCodes] = arrayControl;
+  }
 }
